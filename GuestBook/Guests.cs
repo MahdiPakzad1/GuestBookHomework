444dbaf6fd959e7d36dd967de9d78028fe3a02d7
Mahdi Pakzad <elias-developer@outlook.com>
2023-07-30 10:42:13 +0330

    Add project files.

---
+++ GuestBook/Guests.cs
@@ -0,0 +1,60 @@
+﻿namespace GuestBook;
+
+public static class Guests
+{
+    public static (List<string> guestsList, int total) AskUsersName()
+    {
+        List<string> guestsNameList = new List<string>();
+        string answer = "";
+        int total = 0;
+        do
+        {
+            Console.Write("what is your name : ");
+            string name = Console.ReadLine();
+            guestsNameList.Add(name);
+
+            total += AskForPartyCount();
+
+
+            Console.Write("Is there another guest ? (y/n) : ");
+            answer = Console.ReadLine();
+
+        } while (answer == "y");
+
+        return (guestsNameList, total);
+    }
+
+    public static int AskForPartyCount()
+    {
+
+        bool flag = false;
+        do
+        {
+            Console.Write("How many are in your party: ");
+            string countText = Console.ReadLine();
+            bool isValid = int.TryParse(countText, out int count);
+            if (isValid is false)
+            {
+                Console.WriteLine("Please insert a numeric value");
+
+                flag = true;
+
+            }
+
+            return count;
+
+
+        } while (flag);
+    }
+
+
+
+    public static void PrintGuestsName(List<string> guestsList)
+    {
+        Console.WriteLine("Guests Name list : ");
+        foreach (string name in guestsList)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ GuestBook/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using GuestBook;
+
+(List<string> guestsName, int total) = Guests.AskUsersName();
+Guests.PrintGuestsName(guestsName);
+Console.WriteLine($"Total number of guests is : {total}");
